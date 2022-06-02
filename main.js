47048da6d6096a26044aa01dd5d8f852d5c62a0e
ahmed9aks <97857810+ahmed9aks@users.noreply.github.com>
2022-06-02 10:58:22 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function generateQuote(){
+    fetch('https://animechan.vercel.app/api/random').then(response => response.json()).then(data =>{
+        const quote = data.quote;
+        const character = data.character;
+        const anime = data.anime;
+        document.getElementById("quote").innerHTML= `"${quote}-${character} (${anime})"`;
+    });
+};
